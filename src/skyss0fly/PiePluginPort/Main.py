c006b731a257d4972476148c92f6f66a09116933
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-31 17:17:27 +1000

    Update Main.py

--- src/skyss0fly/PiePluginPort/Main.py
+++ src/skyss0fly/PiePluginPort/Main.py
@@ -1,8 +1,7 @@
 from piemc.plugin.PluginBase import PluginBase
 from piemc.plugin.Terminal import Terminal
 
-   import requests 
-import os
+
 class Main(PluginBase):
   def on_enable(self):
     self.logger.info(Terminal.green("[PiePluginPort] enabling...:)"))
