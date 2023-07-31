dfa59064778f47bfed6730ea93d95dbb3af3f830
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-31 15:54:26 +1000

    Update Main.py

--- src/skyss0fly/PiePluginPort/Main.py
+++ src/skyss0fly/PiePluginPort/Main.py
@@ -7,14 +7,14 @@ class Main(PluginBase):
   def on_enable(self):
     self.logger.info(Terminal.green("[PiePluginPort] enabling..."))
     
- def on_command(self):
-
-url = 'http://www.PieMC-Dev.github.io/Plugins/'
-folder_name = 'Plugins'
-
-if not os.path.exists(folder_name):
-    os.mkdir(folder_name)
-
-for filename in os.listdir(url):
-    r = requests.get(url + filename, allow_redirects=True)
-    open(folder_name + '/' + filename, 'wb').write(r.content)
+ def onCommand(sender, command, label, args):
+  if command == "getplugin":
+    if len(args) == 1:
+      pluginname = args[0]
+      try:
+        plugin = PieMC.getPluginManager().getPlugin(pluginname)
+        sender.sendMessage("Plugin " + pluginname + " found!")
+      except:
+        sender.sendMessage("Plugin " + pluginname + " not found!")
+    else:
+      sender.sendMessage("Usage: /getplugin <pluginname>")
