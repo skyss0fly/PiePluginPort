1c37052705145adc91b0a5b1cdcdb72bd8ee5c87
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-31 15:44:29 +1000

    Update Main.py

--- src/skyss0fly/PiePluginPort/Main.py
+++ src/skyss0fly/PiePluginPort/Main.py
@@ -1,5 +1,5 @@
 from piemc.plugin.PluginBase import PluginBase
-from piemc.plugin.Terminal import terminal
+from piemc.plugin.Terminal import Terminal
 
 class Main(PluginBase):
   def on_enable(self):
